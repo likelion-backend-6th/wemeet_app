262df4490fa41a00ae03b0e27b10ddda13b68b2b
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-09-26 21:59:31 +0900

    EDIT:meet 폴더 통합

--- meet/account/models.py
+++ meet/account/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class UserLocation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    longitude = models.FloatField()  # 경도
+    latitude = models.FloatField()   # 위도
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email}의 위치정보"
