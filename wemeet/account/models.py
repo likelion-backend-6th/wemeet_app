98afc3213177aa5e6f7bb5ba2fa105f65e7a6dbc
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-09-26 16:51:58 +0900

    account user와 userLocation

--- wemeet/account/models.py
+++ wemeet/account/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(db_index=True, unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.email
+class UserLocation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    longitude = models.FloatField()  # 경도
+    latitude = models.FloatField()   # 위도
+    create_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.email}의 위치정보"
