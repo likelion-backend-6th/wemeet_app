da14842d6397be540059f169de417a132ff82a46
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-09-26 20:19:04 +0900

    장고가 제공하는 User모델 사용

--- wemeet/account/models.py
+++ wemeet/account/models.py
@@ -1,17 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class Users(AbstractUser):
-    id = models.AutoField(primary_key=True)
-    email = models.EmailField(db_index=True, unique=True)
-    password = models.CharField(max_length=128)
-
-    def __str__(self):
-        return self.email
+from django.contrib.auth.models import User
 
 class UserLocation(models.Model):
-    location_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     longitude = models.FloatField()  # 경도
     latitude = models.FloatField()   # 위도
     create_at = models.DateTimeField(auto_now_add=True)
--- wemeet/config/settings/base.py
+++ wemeet/config/settings/base.py
@@ -124,4 +124,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-AUTH_USER_MODEL ='account.Users'
+AUTH_USER_MODEL = 'auth.USER'
