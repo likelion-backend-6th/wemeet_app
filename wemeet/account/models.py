4662a277f0b043a1366659569598d2f88456cffa
Juhyeon AN <ajh9789@gachon.ac.kr>
2023-09-26 20:33:29 +0900

    settings.AUTH_USER_MODEL 사용

--- wemeet/account/models.py
+++ wemeet/account/models.py
@@ -1,8 +1,8 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
 
 class UserLocation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     longitude = models.FloatField()  # 경도
     latitude = models.FloatField()   # 위도
     create_at = models.DateTimeField(auto_now_add=True)
