dffa7753747b3e6e3ac2bc15afab54d7efaa3d43
Joey <hanazzang999@gmail.com>
2023-09-26 16:12:11 +0900

    setting 변경: installed app 추가, AUTH_USER_MODEL 값 추가

--- wemeet/config/settings/base.py
+++ wemeet/config/settings/base.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'plan',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'auth.USER'
