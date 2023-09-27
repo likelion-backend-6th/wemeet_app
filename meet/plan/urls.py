e327247d37dfe35ec421b80ec90fe52985744bb0
Younggu Kang <khkkor3@naver.com>
2023-09-27 17:48:22 +0900

    Fix urls

--- meet/plan/urls.py
+++ meet/plan/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
+    path("plans/", views.PlanViewSet.as_view()),
+    path("groups/", views.GroupViewSet.as_view()),
 ]
