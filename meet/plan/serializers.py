462e5b69837ccb4cf248078fb73c8ca00bd35df6
Younggu Kang <khkkor3@naver.com>
2023-09-27 11:30:23 +0900

    merge serializers

--- meet/plan/serializers.py
+++ meet/plan/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Plan
+from .models import Plan, Group
 
 
 class PlanSerializer(serializers.ModelSerializer):
@@ -27,5 +27,26 @@ class PlanSerializer(serializers.ModelSerializer):
         instance.latitude = validated_data.get("latitude", instance.latitude)
         instance.longitude = validated_data.get("longitude", instance.longitude)
         instance.memo = validated_data.get("memo", instance.memo)
+        instance.save()
+        return instance
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = (
+            "id",
+            "plan",
+            "user",
+            "created_at",
+        )
+
+    def create(self, validated_data):
+        group = Group.objects.create(**validated_data)
+        return group
+
+    def update(self, instance, validated_data):
+        instance.plan = validated_data.get("plan", instance.plan)
+        instance.user = validated_data.get("user", instance.user)
         instance.save()
         return instance
