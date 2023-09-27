b51fe43578b0f7da7036530a0e92f92e73c18ab2
Younggu Kang <khkkor3@naver.com>
2023-09-27 17:32:43 +0900

    Create: views

--- meet/plan/views.py
+++ meet/plan/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PlanSerializer, GroupSerializer
+from .models import Plan, Group
+
 
 # Create your views here.
+class PlanViewSet(viewsets.ModelViewSet):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
