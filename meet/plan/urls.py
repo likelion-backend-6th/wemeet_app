41a2af72e497543eb56f5aa712e6d04bcd73d8dc
Younggu Kang <khkkor3@naver.com>
2023-09-27 19:22:30 +0900

    miss?

--- meet/plan/urls.py
+++ meet/plan/urls.py
@@ -1,26 +1,15 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from rest_framework.routers import SimpleRouter, DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r"v", views.PlanViewSet, basename="plan1"),
+router.register(r"v2", views.PlanViewSet2, basename="plan2")
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("account/", include("account.urls")),
-    path("plans/", views.PlanViewSet.as_view()),
-    path("groups/", views.GroupViewSet.as_view()),
+    *router.urls,
 ]
+
+# urlpatterns += [
+#     path('',views.PlanList.as_view(), name='plan3'),
+#     path('api/', views.PlanListAPI.as_view(), name='plan4'),
+# ]
--- meet/plan/views.py
+++ meet/plan/views.py
@@ -19,3 +19,50 @@ class PlanListAPI(APIView):
 
 
 # Create your views here.
+
+
+class PlanList(ListAPIView):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+
+
+# plan list 방법 3: 심플하지만 추상화 되어 있는 방법
+class PlanViewSet(viewsets.ViewSet):
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name="title",
+                in_=openapi.IN_PATH,
+                description="약속 명",
+                required=True,
+                type=openapi.TYPE_STRING,
+            ),
+            openapi.Parameter(
+                name="time",
+                in_=openapi.IN_PATH,
+                description="약속 시간",
+                required=True,
+                type=openapi.TYPE_STRING,
+            ),
+        ]
+    )
+    def list(self, request):
+        queryset = Plan.objects.all()
+        serializer = PlanSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    # uuid로 특정 plan get
+    def retrieve(self, request, pk):
+        queryset = Plan.objects.get(pk=pk)
+        serializer = PlanSerializer(queryset)
+        return Response(serializer.data)
+
+
+class PlanViewSet2(ModelViewSet):
+    queryset = Plan.objects.all()
+    serializer_class = PlanSerializer
+
+
+class GroupViewSet(ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
