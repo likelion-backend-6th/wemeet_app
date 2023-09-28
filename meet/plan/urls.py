63ccdbc5b8675767f693381ac752de0fce4dab75
Joey <hanazzang999@gmail.com>
2023-09-29 01:54:12 +0900

    plan 리스트 뷰 , 상세뷰

--- meet/plan/urls.py
+++ meet/plan/urls.py
@@ -2,13 +2,12 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter,DefaultRouter
 from . import views
 router = DefaultRouter()
-router.register(r'v', views.PlanViewSet, basename='plan1'),
-router.register(r'v2', views.PlanViewSet2, basename='plan2')
+router.register(r'', views.PlanViewSet, basename='plan'),
+
 urlpatterns =[
     *router.urls,
 ]
 
-# urlpatterns += [
-#     path('',views.PlanList.as_view(), name='plan3'),
-#     path('api/', views.PlanListAPI.as_view(), name='plan4'),
-# ]
+urlpatterns += [
+   path('<uuid:pk>/',views.PlanViewSet.as_view({'get':'retrieve'}), name='plan_detail'),
+]
--- meet/plan/views.py
+++ meet/plan/views.py
@@ -4,60 +4,23 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from django.template.response import TemplateResponse
 
 from .models import Plan, Group
 from .serializers import PlanSerializer, GroupSerializer
 
-
-class PlanListAPI(APIView):
-    def get(self,request):
-        qs = Plan.objects.all()
-        serializer = PlanSerializer(qs, many=True)
-        return Response(serializer.data)
-# Create your views here.
-
-class PlanList(ListAPIView):
+class PlanViewSet(ModelViewSet):
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
 
-# plan list 방법 3: 심플하지만 추상화 되어 있는 방법
-class PlanViewSet(viewsets.ViewSet):
-    @swagger_auto_schema(manual_parameters=[
-        openapi.Parameter(
-            name='title',
-            in_=openapi.IN_PATH,
-            description='약속 명',
-            required=True,
-            type=openapi.TYPE_STRING
-
-        ),
-        openapi.Parameter(
-            name='time',
-            in_=openapi.IN_PATH,
-            description='약속 시간',
-            required=True,
-            type=openapi.TYPE_STRING
-
-        )
-    ])
     def list(self,request):
-        queryset = Plan.objects.all()
-        serializer = PlanSerializer(queryset, many=True)
-        return Response(serializer.data)
+        qs = self.get_queryset()
+        return TemplateResponse(request, 'plan/plan_list.html',{'qs':qs})
 
-    # uuid로 특정 plan get
-    def retrieve(self, request, pk):
-        queryset = Plan.objects.get(pk=pk)
-        serializer = PlanSerializer(queryset)
-        return Response(serializer.data)
-
-
-class PlanViewSet2(ModelViewSet):
-    queryset = Plan.objects.all()
-    serializer_class = PlanSerializer
-
+    def retrieve(self,request,pk):
+        qs = self.get_queryset().get(pk=pk)
+        return TemplateResponse(request, 'plan/plan_detail.html', {'qs':qs})
 class GroupViewSet(ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
