81a1f1474578dcecc351426cea1f21b4852826cd
Joey <hanazzang999@gmail.com>
2023-09-29 00:33:08 +0900

    corsheader 설정

--- meet/config/settings/base.py
+++ meet/config/settings/base.py
@@ -54,11 +54,13 @@ INSTALLED_APPS += [
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -163,3 +165,8 @@ SIMPLE_JWT = {
 }
 
 LOGIN_REDIRECT_URL = 'dashboard' #auth.user로 로그인후
+
+CORS_ALLOWED_ORIGINS =[
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
+]
