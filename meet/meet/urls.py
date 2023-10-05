4ac594e9f5925f3e059593caec9390b9884bc93a
Joey <hanazzang999@gmail.com>
2023-10-05 17:00:29 +0900

    main url

--- meet/meet/urls.py
+++ meet/meet/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for meet project.
+URL configuration for config project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,39 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Wemeet",
+        default_version="v1",
+        description="테킷 백엔드스쿨 4팀 프로젝트",
+        # terms_of_service="https://www.google.com/policies/terms/",
+        # contact=openapi.Contact(email="contact@snippets.local"),
+        # license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("account/", include("accountapp.urls")),
+    path("accounts/", include("allauth.urls")),
+    path("plan/", include("plan.urls")),
+    path("auth/", include("auth.urls")),
+    ## API 문서 url
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
