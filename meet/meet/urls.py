e5d5df376aa205fb2d9a69a474b91da4128e2d29
syjoe <csy5501@gmail.com>
2023-10-06 02:05:48 +0900

    UDPATE: static 정적 파일 경로

--- meet/meet/urls.py
+++ meet/meet/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -50,4 +52,4 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
