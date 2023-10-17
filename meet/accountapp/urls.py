7ed6d5c51fafc83daff7e201b24e9959fc4489d3
Younggu Kang <khkkor3@naver.com>
2023-10-17 11:12:04 +0900

    black

--- meet/accountapp/urls.py
+++ meet/accountapp/urls.py
@@ -6,18 +6,29 @@ from . import views
 app_name = "accountapp"
 
 urlpatterns = [
-    path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(template_name='registration/common/password_reset_done.html'),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='registration/common/password_reset_confirm.html',
-             success_url=reverse_lazy('accountapp:password_reset_complete')
-         ), name='password_reset_confirm'),
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='registration/common/password_reset_complete.html'),
-         name='password_reset_complete'),
-    path('password_reset/', views.password_reset_request, name="password_reset"),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="registration/common/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="registration/common/password_reset_confirm.html",
+            success_url=reverse_lazy("accountapp:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="registration/common/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
+    path("password_reset/", views.password_reset_request, name="password_reset"),
     path("", include("django.contrib.auth.urls")),
     # path("login/",LoginView.as_view(),name='login'),
     path("dashboard/", views.dashboard, name="dashboard"),
