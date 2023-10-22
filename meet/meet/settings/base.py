9de77fb2886356f6e7aa200b27aca25c2c9a2730
syjoe <csy5501@gmail.com>
2023-10-23 00:01:13 +0900

    ADD: version test/release/0.11.9

--- meet/meet/settings/base.py
+++ meet/meet/settings/base.py
@@ -17,6 +17,8 @@ from celery.schedules import crontab
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+VERSION = "0.1.0"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
